0e2205479193fc9b8dbb92c4f4aa87824c3c937d
hemanth <ambatihemanthreddy179@gmail.com>
2026-07-15 13:46:06 +0530

    modified

--- app.java
+++ app.java
@@ -2,6 +2,7 @@ public class app {
 	public static void main(string[] args){
 		system.out.println("good morning:");
 		system.out.println("git revert");
-		system.out.println("new file")
+		system.out.println("new file");
+		system.out.println("2nd file")
 
 
