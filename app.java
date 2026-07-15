1d2f824779543e5cb685ff7f474851983c3fe8bc
hemanth <ambatihemanthreddy179@gmail.com>
2026-07-15 13:48:30 +0530

    Revert "modified"
    This reverts commit 0e2205479193fc9b8dbb92c4f4aa87824c3c937d.

--- app.java
+++ app.java
@@ -2,7 +2,6 @@ public class app {
 	public static void main(string[] args){
 		system.out.println("good morning:");
 		system.out.println("git revert");
-		system.out.println("new file");
-		system.out.println("2nd file")
+		system.out.println("new file")
 
 
